0c79ed0edb725e5701b48f733cd39197b08446e2
A.Adefolalu B <adefolalucode101@gmail.com>
2022-06-02 20:33:34 +0100

    create a js file and html file and write a function on js and pprint to console

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// prompting your user to enter a sentence. Then, store the sentence in a variable and print it to the console.
+
+$(document).ready(function(){
+let sentence = prompt("write a sentence on what you need for Ileya ");
+function saySomething (){
+    return " Thank you for telling us what you need "
+
+}
+console.log(saySomething());
+
+});
